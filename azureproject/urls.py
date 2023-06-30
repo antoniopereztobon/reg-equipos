77989ee524913e17766930b05cd08d20c935e6c0
Antonio Perez <antoniopereztobon@gmail.com>
2023-06-30 12:44:41 -0500

    Paso 11

--- azureproject/urls.py
+++ azureproject/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-#from ingreso import views
+from ingreso import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ingreso.urls')),
-    #path("", views.IndexView.as_view(), name="index"),
-    #path("logout/", views.user_logout, name="logout"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("logout/", views.user_logout, name="logout"),
 ]
